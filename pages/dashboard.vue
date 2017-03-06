2984964aae6597cd5368df6e5ece735d33f2c802
Sam Smith <sam@mintcanary.com>
2017-03-06 17:53:03 +0000

    header styling

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,15 +1,20 @@
 <template>
-  <section class="inner">
-    <h2 class="source-title">
-      Test Data
-      <small>github/okfn/test-data</small>
-    </h2>
-    <source-item title="okfn/test-data" integration="github" jobnumber="8"  status="invalid" time="1 day ago" source="active" job="active"/>
-    <div>
-      Report here
-    </div>
-    Added: 21 Feb 2017
-  </section>
+  <div>
+    <header class="main-header">
+      
+    </header>
+    <section class="inner">
+      <h2 class="source-title">
+        Test Data
+        <small>github/okfn/test-data</small>
+      </h2>
+      <source-item title="okfn/test-data" integration="github" jobnumber="8"  status="invalid" time="1 day ago" source="active" job="active"/>
+      <div>
+        Report here
+      </div>
+      Added: 21 Feb 2017
+    </section>
+  </div>
 </template>
 
 <script>
