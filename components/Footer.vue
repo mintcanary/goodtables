7b2d2aaf52953338a25c3c1d6e90f0bf735952c7
Sam Smith <sam@mintcanary.com>
2017-05-02 11:12:14 +0100

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,19 @@
 <template>
-  <footer>
+  <footer class="site-footer">
     <div class="container">
-      <!-- Footer -->
+      <div class="footer-primary">
+        <a class="footer-logo" href="https://okfn.org/">
+          <img src="https://a.okfn.org/img/oki/landscape-white-468x122.png" alt="Open Knowledge International">
+        </a>
+        <ul class="footer-links">
+          <li><a href="https://twitter.com/OKFNLabs">@OKFNLabs</a></li>
+        </ul>
+      </div>
+      <div class="footer-secondary">
+        <p>
+          &copy; <a href="https://okfn.org">Open Knowledge International</a> 2017
+        </p>
+      </div>
     </div>
   </footer>
 </template>
