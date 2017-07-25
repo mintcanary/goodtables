5c8cf3769243eebed61a863ae97d545b6d75fa4d
Sam Smith <sam@mintcanary.com>
2017-07-25 09:27:31 +0100

    add icons to nav

--- components/primary_nav.vue
+++ components/primary_nav.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="nav primary">
     <li class="active">
-      <a href="#">Dashboard</a>
+      <a href="#"><span class="icon-dashboard" aria-hidden="true"></span> Dashboard</a>
     </li>
     <li>
-      <a href="#">Manage Sources</a>
+      <a href="#"><span class="icon-equalizer" aria-hidden="true"></span> Manage Sources</a>
     </li>
   </ul>
 </template>
