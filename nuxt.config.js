408300d1e31d513ddea3f8b5a69dcf34efd0bae2
Sam Smith <sam@mintcanary.com>
2017-10-23 16:20:12 +0100

    router doesn't work inside build

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,10 @@ module.exports = {
     { src: '~assets/scss/main.scss', lang: 'scss' }
   ],
 
+  router: {
+    base: '/goodtables/'
+  },
+
   plugins: ['~/plugins/scrollto'],
 
   /*
@@ -36,10 +40,6 @@ module.exports = {
   ** Build configuration
   */
   build: {
-
-    router: {
-      base: '/goodtables/'
-    },
 
     vendor: ['jquery', 'bootstrap-sass'],
     plugins: [
