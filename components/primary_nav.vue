029655107a2e728553a4d68b8759d8332347f63e
Sam Smith <sam@mintcanary.com>
2017-10-23 17:25:49 +0100

    add jobs menu item

--- components/primary_nav.vue
+++ components/primary_nav.vue
@@ -1,10 +1,13 @@
 <template>
   <ul class="nav primary">
-    <router-link tag="li" to="/dashboard" exact-active-class="active">
-      <a><span class="icon-dashboard" aria-hidden="true"></span> Dashboard</a>
+    <router-link tag="li" to="/dashboard" exact-active-class="active" active-class="active-parent">
+      <a><span class="icon-keyboard_arrow_left back-icon" aria-hidden="true"></span><span class="icon-dashboard section-icon" aria-hidden="true"></span> Dashboard</a>
     </router-link>
-    <router-link tag="li" to="/sources" exact-active-class="active">
-      <a><span class="icon-equalizer" aria-hidden="true"></span> Manage Sources</a>
+    <router-link tag="li" to="/jobs" exact-active-class="active" active-class="active-parent">
+      <a><span class="icon-keyboard_arrow_left back-icon" aria-hidden="true"></span><span class="icon-jobs section-icon" aria-hidden="true"></span> Jobs</a>
+    </router-link>
+    <router-link tag="li" to="/sources" exact-active-class="active" active-class="active-parent">
+      <a><span class="icon-keyboard_arrow_left back-icon" aria-hidden="true"></span><span class="icon-equalizer section-icon" aria-hidden="true"></span> Manage Sources</a>
     </router-link>
   </ul>
 </template>
---
+++ pages/jobs/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+    Jobs
+
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'app',
+    head () {
+      return {
+        title: `goodtables`
+      }
+    }
+  }
+</script>
