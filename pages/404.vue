7d08a4d7e0d9b8dc0c9f9e2546941f747d4ae98b
Sam Smith <sam@mintcanary.com>
2017-11-07 11:26:32 +0000

    static 404 example

---
+++ pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="error-page">
+    <section class="inner">
+      <h1>
+        This page could not be found (404)
+      </h1>
+      <maze/>
+      <logo/>
+    </section>
+  </div>
+</template>
+<script>
+import Maze from '~/components/maze.vue'
+import Logo from '~/components/Logo.vue'
+
+export default {
+  props: ['error'],
+  components: {
+    Maze,
+    Logo
+  },
+  layout: 'minimal'
+}
+</script>
